8a68eb29d7283dce9e87ac51bee00c1666a301de
mamanhinal <maryamaliyumuhammad6t@gmail.com>
2026-03-09 00:04:16 +0100

    Create maryam.java

---
+++ maryam.java
@@ -0,0 +1,8 @@
+class maryam {
+    public static void main(String[] args) {
+        // maryam info
+        System.out.println("Maryam Aliyu Muhammad");
+        System.out.println("maryamaliyumuhammad6t@gmail.com");
+        System.out.println("Proteomics");
+    }
+}
