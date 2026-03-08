8303bd6f720b2f2f3cfe3f53c9856e448dd56817
hussainifadila259-spec <hussainifadila259@gmail.com>
2026-03-08 14:39:21 +0100

    Create Fadila.pl

---
+++ Fadila.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+# Script for Fadila Hassaini Kupgai
+# Course: ANA 4315 - Introduction to Bioinformatics
+
+print "Fadila Hassaini kupgai\n";
+print "hussainifadila259@gmail.com\n";
+print "Hussainifadila259\n";
+print "Transcriptomics\n";
