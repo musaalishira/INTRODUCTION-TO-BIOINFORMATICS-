d9ee0f71c41d2d94d8728ddba2f528628c030209
hassanmalamauwal-cmd <hassanmalamauwal@gmail.com>
2026-03-08 14:22:12 +0100

    Create Hassan.R

---
+++ Hassan.R
@@ -0,0 +1,7 @@
+# Script for Hassan Malam Auwal
+# Course: ANA 4315 - Introduction to Bioinformatics
+
+cat("Hassan Malam Auwal", "\n")
+cat("hassanmalamauwal@gmail.com", "\n")
+cat("Akarami", "\n")
+cat("Proteomics", "\n")
