476b981184693da6892a15676222f635509e5708
Istifanus <istifanusirimiya935@gmail.com>
2026-03-08 14:54:32 +0100

    Create Istifahanus.cpp with basic output
    Add initial implementation for Istifahanus program

---
+++ Istifahanus.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Script for Istifanus Irimiya
+    // Course: ANA 4315 - Introduction to Bioinformatics
+
+    cout << "Istifanus Irimiya" << endl;
+    cout << "istifanusirimiya935@gmail.com" << endl;
+    cout << "Istifahanusx" << endl;
+    cout << "Metagenomics" << endl;
+
+    return 0;
