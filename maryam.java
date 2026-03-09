52d769eb8f7768f3f7cc6fd7a885b1bea8d0c1e1
musaalishira <musaalishira2023@gmail.com>
2026-03-09 11:29:02 +0100

    Delete maryam.java

--- maryam.java
+++
@@ -1,8 +0,0 @@
-class maryam {
-    public static void main(String[] args) {
-        // maryam info
-        System.out.println("Maryam Aliyu Muhammad");
-        System.out.println("maryamaliyumuhammad6t@gmail.com");
-        System.out.println("Proteomics");
-    }
-}
