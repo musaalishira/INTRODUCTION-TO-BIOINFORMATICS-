40532addd329872cba17e471a7ebd7e44e741771
musaalishira <musaalishira2023@gmail.com>
2026-03-09 11:27:27 +0100

    Delete Fadila.pl

--- Fadila.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-# Script for Fadila Hassaini Kupgai
-# Course: ANA 4315 - Introduction to Bioinformatics
-
-print "Fadila Hassaini kupgai\n";
-print "hussainifadila259@gmail.com\n";
-print "Hussainifadila259\n";
-print "Transcriptomics\n";
