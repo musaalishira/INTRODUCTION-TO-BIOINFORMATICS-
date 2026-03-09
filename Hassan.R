9e8b6a18c46d8dfd9bd6a05f89eeaeaecf3c4943
musaalishira <musaalishira2023@gmail.com>
2026-03-09 11:28:02 +0100

    Delete Hassan.R

--- Hassan.R
+++
@@ -1,7 +0,0 @@
-# Script for Hassan Malam Auwal
-# Course: ANA 4315 - Introduction to Bioinformatics
-
-cat("Hassan Malam Auwal", "\n")
-cat("hassanmalamauwal@gmail.com", "\n")
-cat("Akarami", "\n")
-cat("Proteomics", "\n")
